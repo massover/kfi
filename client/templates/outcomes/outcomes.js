72d07a8711f813db63e42938f8107211064aa986
Joshua Massover <joshua.massover@gmail.com>
2015-02-14 22:01:14 -0500

    more

--- client/templates/outcomes/outcomes.js
+++ client/templates/outcomes/outcomes.js
@@ -1,12 +1,21 @@
 Template.outcomes.helpers({
-    outcome: function(){
-        console.log(this.people);
-        for (var index in this.people){
-            person = this.people[index];
-            console.log(person);
-            person = People.findOne({_id:person.personId});
-            console.log(person);
+    people: function(){
+        people = this.people
+        for (var index in people){
+            person = People.findOne({_id: people[index].personId});
+            people[index].img_path = person.img_path;
+            total_decisions = people[index].kill + 
+                              people[index].fuck + 
+                              people[index].impeach;
+            people[index].kill_percentage = 
+                Math.round((people[index].kill / total_decisions) * 100);
+            people[index].fuck_percentage = 
+                Math.round((people[index].fuck / total_decisions) * 100);
+            console.log(people[index].kill_percentage);
+            people[index].impeach_percentage = 
+                Math.round((people[index].impeach / total_decisions) * 100);
         }
+        return people;
     }
 
 });
