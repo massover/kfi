ce6bd4d248426f295cd0bd2799b2416dc5c19713
Joshua Massover <joshua.massover@gmail.com>
2015-02-28 18:43:15 -0500

    using asbolute paths instead of relative paths for images

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,14 +1,14 @@
 if (People.find().count() === 0){
     People.insert({
-        img_path: 'images/barack_obama.png',
+        img_path: '/images/barack_obama.png',
         name: 'Barack Obama'
     });
     People.insert({
-        img_path: 'images/adolf_hitler.jpg',
+        img_path: '/images/adolf_hitler.jpg',
         name: 'Adolf Hitler'
     });
     People.insert({
-        img_path: 'images/george_w_bush.jpg',
+        img_path: '/images/george_w_bush.jpg',
         name: 'George W Bush'
     });
 }
